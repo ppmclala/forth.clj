456458196c8075f206eaa06d529dd6acedcebded
mrm <mattymcmanus@gmail.com>
2024-03-10 18:54:18 -0400

    consolidate into one dictionary.  clean up execution code

--- src/forth.clj
+++ src/forth.clj
@@ -1,14 +1,81 @@
 (ns forth
   (:import [java.util Scanner]))
 
+(defn !push [ds & tokens]
+  (swap! ds (fn [s t] (apply conj s t)) tokens))
+
+(defn !pop [ds]
+  (let [v (first @ds)] (swap! ds (fn [s] (rest s))) v))
+
+(defn !peek [ds] (first @ds))
+
+(defn- begin-comment [m] (reset! m :comment))
+(defn- end-comment [m] (reset! m :interpret))
+(defn- start-compiling [m] (reset! m :compile))
+(defn- stop-compiling [{:keys [mode compile-target]}]
+  (reset! mode :interpret)
+  (reset! compile-target nil))
+
+;; intrinsics
+(def dictionary
+  {:.
+   (fn [s _ _] (println (!pop s)))
+
+   :+
+   (fn [s _ _] (!push s (+ (!pop s) (!pop s))))
+
+   :-
+   (fn [s _ _]
+     (let [*1 (!pop s)
+           *2 (!pop s)]
+       (!push s (- *2 *1))))
+   :*
+   (fn [s _ _] (!push s (* (!pop s) (!pop s))))
+
+   :DIV
+   (fn [s _ _]
+     (let [*1 (!pop s)
+           *2 (!pop s)]
+       (!push s (/ *2 *1))))
+
+   :DUP
+   (fn [s _ _] (!push s (!peek s)))
+
+   :SWAP
+   (fn [s _ _]
+     (let [*1 (!pop s)
+           *2 (!pop s)]
+       (!push s *1 *2)))
+
+   :ROT
+   (fn [s _ _]
+     (let [*1 (!pop s)
+           *2 (!pop s)
+           *3 (!pop s)]
+       (!push s *3 *1 *2)))
+   :DROP
+   (fn [s _ _] (!pop s))
+
+   :BEGIN_COMMENT
+   (fn [_ _ m] (begin-comment m))
+
+   :END_COMMENT
+   (fn [_ _ m] (end-comment m))
+
+   :DOCOL
+   (fn [_ _ m] (start-compiling m))
+
+   :EXIT
+   (fn [_ _ m] (stop-compiling m))})
+
 (def ^:dynamic *debug* false)
-(defn debug [{:keys [stack dict mode compile-target custom-dict] :as machine}]
+(defn debug [{:keys [stack mode dict compile-target]}]
   (when *debug*
     (println "Current machine: ")
     (println "\tstack:      top->" @stack)
     (println "\tmode:            " @mode)
     (when (= :compile @mode)
-      (let [instrs (get @custom-dict @compile-target)]
+      (let [instrs (get @dict @compile-target)]
         (println "\tcompile-target:  " @compile-target)
         (println "\tinstructions:    " (when instrs @instrs))))))
 
@@ -23,19 +90,9 @@
         (catch Exception _
           (keyword raw-token))))))
 
-(defn !push [ds & tokens]
-  (swap! ds (fn [s t] (apply conj s t)) tokens))
+(defn- match-kw [kw t] (= t (keyword kw)))
 
-(defn !pop [ds]
-  (let [v (first @ds)]
-    (swap! ds (fn [s] (rest s)))
-    v))
-
-(defn !peek [ds] (first @ds))
-
-(defn match-kw [kw t] (= t (keyword kw)))
-
-(defn token->word [t]
+(defn- token->word [t]
   (condp match-kw t
     "/" :DIV
     "(" :BEGIN_COMMENT
@@ -44,87 +101,47 @@
     ";" :EXIT
     t))
 
-(defn- begin-comment [m] (reset! m :comment))
-(defn- end-comment [m] (reset! m :interpret))
-(defn- start-compiling [m] (reset! m :compile))
-(defn- stop-compiling [{:keys [mode compile-target]}]
-  (reset! mode :interpret)
-  (reset! compile-target nil))
-
-;; intrinsics
-(def native-words
-  {:. (fn [s _ _] (println (!pop s)))
-   :+ (fn [s _ _] (!push s (+ (!pop s) (!pop s))))
-   :- (fn [s _ _]
-        (let [*1 (!pop s)
-              *2 (!pop s)]
-          (!push s (- *2 *1))))
-   :* (fn [s _ _] (!push s (* (!pop s) (!pop s))))
-   :DIV (fn [s _ _]
-          (let [*1 (!pop s)
-                *2 (!pop s)]
-            (!push s (/ *2 *1))))
-
-   :DUP (fn [s _ _] (!push s (!peek s)))
-   :SWAP (fn [s _ _]
-           (let [*1 (!pop s)
-                 *2 (!pop s)]
-             (!push s *1 *2)))
-   :ROT (fn [s _ _]
-          (let [*1 (!pop s)
-                *2 (!pop s)
-                *3 (!pop s)]
-            (!push s *3 *1 *2)))
-   :DROP (fn [s _ _] (!pop s))
-   :BEGIN_COMMENT (fn [_ _ m] (begin-comment m))
-   :END_COMMENT (fn [_ _ m] (end-comment m))
-   :DOCOL (fn [_ _ m] (start-compiling m))
-   :EXIT (fn [_ _ m] (stop-compiling m))})
-
-(defn initialize []
-  {:stream (Scanner. System/in)
-   :stack (atom (list))
-   :mode (atom :interpret) ; maybe :eval?
-   :compile-target (atom nil)
-   :custom-dict (atom {})
-   :dict (atom native-words)})
-
 (defn- unhandled-word [m w]
   (println "Don't know how to handle word " w)
   (print-dict m))
 
-(defn- set-compile-target [compile-target custom-dict word]
+(defn- set-compile-target [compile-target dict word]
   (reset! compile-target word)
-  (swap! custom-dict assoc word (atom [])))
+  (swap! dict assoc word (atom [])))
 
 (defn- compile-word [instructions w]
   (conj instructions w))
 
-(defn- compile-next [{:keys [compile-target custom-dict]} word]
+(defn- compile-next [{:keys [compile-target dict]} word]
   (if-not @compile-target
-    (set-compile-target compile-target custom-dict word)
-    (swap! (get @custom-dict @compile-target) compile-word word)))
+    (set-compile-target compile-target dict word)
+    (swap! (get @dict @compile-target) compile-word word)))
 
-(defn- custom-instrs [{:keys [custom-dict]} word]
-  (get @custom-dict word))
+(defn- compiled? [h] (= (type h) clojure.lang.Atom))
+(defn- native? [h] (fn? h))
 
 (defn- exec-instrs* [{:keys [dict stack mode] :as m} instrs]
   (let [next-word (first instrs)
-        custom-instrs (custom-instrs m next-word)
-        rest-instrs (rest instrs)]
-    (if custom-instrs
-      (do
-        (exec-instrs* m @custom-instrs)   ;; execute custom word
-        (exec-instrs* m rest-instrs))   ;; continue with next instructions
-      (let [native-fn (get @dict next-word)]
-        (if native-fn
-          (do
-            (native-fn stack dict mode)
-            (exec-instrs* m rest-instrs))
-          (when next-word 
-            (unhandled-word m next-word)))))))
+        code-word (get @dict next-word)]
 
-(defn- next-word [{:keys [stack dict mode] :as m} w]
+    (cond
+
+      (compiled? code-word)
+      ;; code-word is a vector of instructions (wrapped in an Atom)
+      (do
+        (exec-instrs* m @code-word)
+        (exec-instrs* m (rest instrs)))
+
+      (native? code-word)
+      ;; code-word is a fn
+      (do
+        (code-word stack dict mode)
+        (exec-instrs* m (rest instrs)))
+
+      (some? next-word)
+      (unhandled-word m next-word))))
+
+(defn- next-word [{:keys [mode] :as m} w]
   (case @mode
     :comment
     (when (= w :END_COMMENT)
@@ -138,19 +155,26 @@
       (stop-compiling m)
       (compile-next m w))))
 
-(defn forth-eval [{:keys [stack] :as machine} t]
+(defn- forth-eval [{:keys [stack] :as machine} t]
   (if (instance? Long t)
     (!push stack t)
     (next-word machine (token->word t)))
   machine)
 
-(defn eval-line [machine line]
+(defn- eval-line [machine line]
   (let [scanner (Scanner. line)]
     (loop [m machine
            t (next-token scanner)]
       (if t (recur (forth-eval m t) (next-token scanner)) m))))
 
 (defn- next-line [{:keys [stream]}] (. stream nextLine))
+
+(defn initialize []
+  {:stream (Scanner. System/in)
+   :stack (atom (list))
+   :mode (atom :interpret) ; maybe :eval?
+   :compile-target (atom nil)
+   :dict (atom dictionary)})
 
 (defn repl [machine]
   (with-bindings {#'*debug* true}
@@ -166,13 +190,6 @@
 (comment
 
   (repl (initialize))
-
-  ;; test case:
-  ;; : SQ ( n -- s ) DUP * ;
-  ;; 2 SQ ;-> 4
-
-  ;; TODO:
-  ;; - [ ] implment DOCOL (implies compiling mode)
 
   ;;
   )
